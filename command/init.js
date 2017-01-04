049c23bfa4a8df1fbae04231d0fe941c98ffd2d3
zhonggithub <1006817093@qq.com>
2017-01-04 22:11:50 +0800

    增加工程文件

---
+++ command/init.js
@@ -0,0 +1,31 @@
+'use strict'
+const exec = require('child_process').exec
+const co = require('co')
+const prompt = require('co-prompt')
+const config = require('../templates')
+const chalk = require('chalk')
+
+module.exports = () => {
+ 	co(function *() {
+  	let projectName = yield prompt('Project name: ')
+  	let gitUrl
+  	let branch
+
+		gitUrl = config.tpl.url
+		branch = config.tpl.branch
+
+    let cmdStr = `git clone -b ${branch} ${gitUrl} ${projectName}`
+
+	  console.log(chalk.white('\n Start generating...'))
+
+	  exec(cmdStr, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error)
+        process.exit()
+      }
+      console.log(chalk.green('\n √ Generation completed!'))
+      console.log(`\n cd ${projectName} && npm install \n`)
+      process.exit()
+	  })
+  })
+}
