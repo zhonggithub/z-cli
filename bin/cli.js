d30d075044c018ac2d2d72aedad4eb82fc42dff8
Zz <quitjie@gmail.com>
2018-07-28 16:36:29 +0800

    Fix

--- bin/cli.js
+++ bin/cli.js
@@ -6,14 +6,14 @@ process.env.NODE_PATH = __dirname + '/../node_modules/'
 const program = require('commander')
 
 program
-	.version(require('../package').version )
-
+  .version(require('../package').version)
+  
 program
 	.usage('<command>')
 
 program
   .command('init <template>')
-	.description('Generate a new project')
+	.description('使用哪个框架初始化工程？可选：z-react-koa, z-react-koa@next, z-app, z-react, z-react@next, z-seneca')
   .alias('i')
   .action((template) => {
     require('../command/init')(template)
